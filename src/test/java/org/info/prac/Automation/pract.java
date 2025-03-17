11b0521ca32ae52f29b581d14059eb801fd518ca
Arul Nidhi Kalyanasundaram <arulnidhi.k@ad.infosys.com>
2025-03-17 09:34:48 +0530

    maven prac

---
+++ src/test/java/org/info/prac/Automation/pract.java
@@ -0,0 +1,18 @@
+package org.info.prac.Automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class pract {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().minimize();
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		System.out.println("executed successfully");
+		driver.close();
+	}
+}
